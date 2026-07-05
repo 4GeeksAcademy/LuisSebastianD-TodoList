67157bae945381e3909af50c4ff6f70b0792c17b
LuisSebastianD <luis_s956@hotmail.com>
2026-07-05 02:28:04 +0000

    Corrección: según Lineamientos de Código

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,20 +1,36 @@
 import { useState } from "react";
 
 const TodoList = () => {
-    const [tareas, setTareas] = useState([]);
-    const [botonEliminar, setBotonEliminar] = useState([]);
+    const [taskList, setTaskList] = useState([]);
+    const [task, setTask] = useState('');
 
-    const handleEliminarTarea = (event) => {
-        if (event.target.className === "fa-solid fa-xmark text-danger")
-            setTareas(tareas.filter((tarea, index) => index !== parseInt(event.currentTarget.id)));
+    const handleGetTask = (event) => {
+        if (event.key === 'Enter' && event.target.value !== '') {
+            setTaskList([...taskList, task]);
+            event.target.value = '';
+        }
     }
 
-    const handleMostrarBotonEliminar = (event) => {
-        setBotonEliminar(botonEliminar.map((valor, index) => (index === parseInt(event.currentTarget.id) ? true : false)));
+    const handleDeleteTask = (event) => {
+        if (event.target.className === "delete fa-solid fa-xmark text-danger")
+            setTaskList(taskList.filter((tarea, index) => index !== parseInt(event.currentTarget.id)));
     }
 
-    const handleQuitarBotonEliminar = () => {
-        setBotonEliminar(botonEliminar.map(() => false));
+    const renderTask = () => {
+        if (taskList.length === 0) {
+            return <li className="list-group-item rounded-0 d-flex justify-content-between ps-5 fs-5">
+                No hay tareas, añadir tareas</li>
+        }
+        return taskList.map((tarea, index) => {
+            return <li
+                key={crypto.randomUUID()}
+                id={index}
+                className="list-group-item rounded-0 d-flex justify-content-between ps-5 fs-5"
+                onClick={(event) => handleDeleteTask(event)}>
+                {tarea}
+                <i className="delete fa-solid fa-xmark text-danger"></i>
+            </li>;
+        })
     }
 
     return (
@@ -26,30 +42,14 @@ const TodoList = () => {
                         className="list-group-item rounded-0 ps-5 fs-5"
                         type="text"
                         placeholder="Tarea por hacer"
-                        onKeyDown={(event) => {
-                            if (event.key === 'Enter' && event.target.value !== '') {
-                                setTareas([...tareas, event.target.value]);
-                                setBotonEliminar([...botonEliminar, false]);
-                                event.target.value = '';
-                            }
-                        }
-                        } />
-                    {tareas.length === 0 ? <li className="list-group-item rounded-0 d-flex justify-content-between ps-5 fs-5">
-                        No hay tareas, añadir tareas</li> :
-                        tareas.map((tarea, index) => {
-                            return <li
-                                key={crypto.randomUUID()}
-                                id={index}
-                                className="list-group-item rounded-0 d-flex justify-content-between ps-5 fs-5"
-                                onMouseEnter={(event) => handleMostrarBotonEliminar(event)}
-                                onMouseLeave={handleQuitarBotonEliminar}
-                                onClick={(event) => handleEliminarTarea(event)}>
-                                {tarea}{botonEliminar[index] === true ? <i className="fa-solid fa-xmark text-danger"></i> : ''}
-                            </li>;
-                        })}
+                        onChange={(event) => setTask(event.target.value)}
+                        onKeyDown={(event) => handleGetTask(event)} />
+                    {renderTask()}
                 </ul>
                 <div className="card-footer">
-                    <h6 className="m-0 fs-6 fw-light text-body-tertiary">{tareas.length} item left</h6>
+                    <h6 className="m-0 fs-6 fw-light text-body-tertiary">
+                        {taskList.length} item left
+                    </h6>
                 </div>
             </div>
         </div>
